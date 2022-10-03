ffa17aebbdc8deb36d96213078a2160ab0590392
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-03 07:29:25 +0300

    commit tree 25bece979aeff7ef2da666f4d0fe8fc2bc15d285

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
+
+    def show
+        item =Item.find(params[:id])
+        render json: item, include: :users
+    end
+
 end
